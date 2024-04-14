f3289ca32d3216a5e113b330aed29708f126f05f
salmannhr <salman.hamalarizky@gmail.com>
2024-04-15 00:51:23 +0700

    first commit bebas

---
+++ salman_tpmodul8_1302220122/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace salman_tpmodul8_1302220122
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
